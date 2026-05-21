93f28c8cd72811078ca5702193be5173965e37c8
Lloyd dela Cruz <lloyd.delacruz@outlook.com>
2026-05-21 13:15:42 -0700

    requirements added to the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Lloyd dela Cruz",
+    author_email="lloyd.delacruz@outlook.com",
+    packages=find_packages(),
+    install_requires=[]
+)
